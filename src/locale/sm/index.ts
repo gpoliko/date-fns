741dc5e69bd3173844631ef2d77b3a235aae5dfc
Giovanni Poliko <60165179+gpoliko@users.noreply.github.com>
2021-09-14 19:21:52 +1200

    chore: start adding Samoan locale Copy en-US as a base

---
+++ src/locale/sm/index.ts
@@ -0,0 +1,29 @@
+import formatDistance from './_lib/formatDistance/index'
+import formatLong from './_lib/formatLong/index'
+import formatRelative from './_lib/formatRelative/index'
+import localize from './_lib/localize/index'
+import match from './_lib/match/index'
+import type { Locale } from '../types'
+
+/**
+ * @type {Locale}
+ * @category Locales
+ * @summary Samoan locale
+ * @language Samoan
+ * @iso-639-2 sm
+ * @author Giovanni Poliko [@gpoliko]{@link https://github.com/gpoliko}
+ */
+const locale: Locale = {
+  code: 'sm-SM',
+  formatDistance: formatDistance,
+  formatLong: formatLong,
+  formatRelative: formatRelative,
+  localize: localize,
+  match: match,
+  options: {
+    weekStartsOn: 1 /* Monday */,
+    firstWeekContainsDate: 4,
+  },
+}
+
+export default locale
